26fb522e0c157764798d90f2b6203e85d59a92d2
roberto <rcastellong98@gmail.com>
2025-07-05 22:52:25 +0200

    vpc module provisioned

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "MyVPC"
+  cidr = "192.168.0.0/26"
+
+  azs             = ["us-east-1a"]
+  public_subnets  = ["192.168.0.0/27"]
+  private_subnets = ["192.168.0.32/27"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
